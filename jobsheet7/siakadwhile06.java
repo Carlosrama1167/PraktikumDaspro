b5dfedcbe1d4eb7dd11515d246e950f86a916d80
carlosrama <carlosrama1144@gmail.com>
2025-10-23 11:59:18 +0700

    modifikasi percobaan 2

--- jobsheet7/siakadwhile06.java
+++ jobsheet7/siakadwhile06.java
@@ -10,11 +10,11 @@ public class siakadwhile06 {
         int jml;
         int i = 0;
 
-        System.out.println("Masukkan jumlah mahasiswa: ");
+        System.out.print("Masukkan jumlah mahasiswa: ");
         jml = input.nextInt();
-
+        
         while (i < jml){
-            System.out.println("Masukkan nilai mahsiswa ke-" + (i + 1) + ":");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ":");
             nilai = input.nextInt();
 
             if (nilai < 0 || nilai > 100){
@@ -23,21 +23,25 @@ public class siakadwhile06 {
             }
 
             if (nilai > 80 && nilai <= 100) {
-                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah A");
+                System.out.println("nilai mahasiswa ke-" + (i+1) + " adalah A");
             } else if (nilai > 73 && nilai <= 80) {
-                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah B+");
+                System.out.println("nilai mahasiswa ke-" + (i+1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
-                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah B");
+                System.out.println("nilai mahasiswa ke-" + (i+1) + " adalah B");
             } else if (nilai > 60 && nilai <= 65) {
-                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah C+");
+                System.out.println("nilai mahasiswa ke-" + (i+1) + " adalah C+");
             }else if (nilai > 50 && nilai <= 60) {
-                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah C");
+                System.out.println("nilai mahasiswa ke-" + (i+1) + " adalah C");
             } else if (nilai > 39 && nilai <= 50) {
-                System.out.println("nilai mahasiswa ke-" + (i+1) + "adalah D");
+                System.out.println("nilai mahasiswa ke-" + (i+1) + " adalah D");
             } else {
-                System.out.println("Nilai mahasiswa ke-" + (i+1) + "adalah E");
+                System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah E");
             }
-            i++;
+
+            if (nilai > 80 && nilai <= 100){
+                System.out.println("Bagus, Pertahankan nilainya");
+            }
+            i++;  
         }
     }
 }
