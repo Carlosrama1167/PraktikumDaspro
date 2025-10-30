12ae5639c795cc9b877f3df423f7455a8c012497
carlosrama <carlosrama1144@gmail.com>
2025-10-30 11:43:20 +0700

    percobaan 3

--- jobsheet8/triangle06.java
+++ jobsheet8/triangle06.java
@@ -2,19 +2,22 @@ package jobsheet8;
 
 import java.util.Scanner;
 
-public class square06 {
+public class triangle06 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Masukkan nilai N= ");
         int N = input.nextInt();
 
-        for(int iOuter=1; iOuter<=N; iOuter++){
-            for (int i=1; i<=N; i++){
-            System.out.print("*");
+        int i = 0;
+        while(i <= N){
+            int j = 0;
+            while(j < i){
+                System.out.print("*");
+                j++;
             }
-            System.out.println();
+            
+            i++;
         }
-        
     }
 }
