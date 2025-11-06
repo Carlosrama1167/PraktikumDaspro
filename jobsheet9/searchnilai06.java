7b9874c836d115f8c99ae4759fcafb1f7ca1fd4e
carlosrama <carlosrama1144@gmail.com>
2025-11-06 13:21:29 +0700

    modifikasi percobaan 4

--- jobsheet9/searchnilai06.java
+++ jobsheet9/searchnilai06.java
@@ -1,19 +1,38 @@
 package jobsheet9;
 
+import java.util.Scanner;
+
 public class searchnilai06 {
     public static void main(String[] args) {
-        int [] arrnilai = {80, 85, 78, 96,  90, 82, 86};
-        int key = 90;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int nilai = input.nextInt();
+
+        int [] arrnilai = new int [nilai];
+        int key ;
         int hasil = 0;
 
         for (int i = 0; i < arrnilai.length; i++) {
+            System.out.print("Masukkan nilai mahsiswa ke-" + (i+1) + " : ");
+            arrnilai[i]= input.nextInt();
+        }
+        System.out.print("Masukkan nilai yang di cari: ");
+        key = input.nextInt();
+        for (int i = 0; i < arrnilai.length; i++) {
            if (key == arrnilai[i]) {
                 hasil = i;
-                break;
+                System.out.println("\nnilai "+ key+ " ketemu di indeks ke-" + hasil);
+               
+           } else {
+                hasil = -1;
+                System.out.print("\nnilai di cari tidak ditemukan");
+                
            }
+           break;
         }
+        
         System.out.println();
-        System.out.println("nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        
     }
 }
