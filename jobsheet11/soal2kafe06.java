4ed7a630f341df4ad72f75d3454f1a7ea323d8ad
carlosrama <carlosrama1144@gmail.com>
2025-11-27 12:25:24 +0700

    modifikasi percobaan 2

--- jobsheet11/soal2kafe06.java
+++ jobsheet11/soal2kafe06.java
@@ -2,15 +2,29 @@ package jobsheet11;
 
 public class soal2kafe06 {
     public static void main(String[] args) {
-      menu("Rama", true);  
+      menu("Rama", true , "DISKON50");  
+        
+      System.out.println("------------------------------");
+      menu("Budi", false , "DISKON30");
+
+      System.out.println("------------------------------");
+        menu("Andi", false , "");
     }
-    public static void menu(String namapelanggan, boolean ismember) {
+    public static void menu(String namapelanggan, boolean ismember, String kodepromo) {
         System.out.println("Selama datang, " + namapelanggan + "!");
 
         if (ismember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian.");
         } 
         
+        if (kodepromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid! Dapatkan diskon tambahan 50%.");
+        } else if (kodepromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid! Dapatkan diskon tambahan 30%.");
+        }else {
+            System.out.println("Kode promo tidak valid.");
+        }
+
         System.out.println("====== Menu Resto Kafe ======");
         System.out.println("1. Kopi Hitama - Rp 15.000");
         System.out.println("2. Capuccino - Rp 20.000");
