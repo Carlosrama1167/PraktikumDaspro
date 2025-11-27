a1e5f5aa458aadfe74604f846b326108c148a017
carlosrama <carlosrama1144@gmail.com>
2025-11-27 13:40:31 +0700

    modifikasi percobaan 4

--- jobsheet11/pengunjungcafe06.java
+++ jobsheet11/pengunjungcafe06.java
@@ -6,6 +6,11 @@ public class pengunjungcafe06 {
         for(int i = 0; i < namapengunjung.length; i++){
             System.out.println("- " + namapengunjung[i]);
         }
+        System.out.println("Menggunakan for each:");
+        for(String nama : namapengunjung){ 
+            System.out.println("- " + nama);
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         daftarpengunjung("ali", "Budi", "Citra");
