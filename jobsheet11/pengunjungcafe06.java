e0eaef771b0b1ddbcc1203c79a25594ec3663a7b
carlosrama <carlosrama1144@gmail.com>
2025-11-27 13:19:33 +0700

    percobaan 4

--- jobsheet11/pengunjungcafe06.java
+++ jobsheet11/pengunjungcafe06.java
@@ -1,5 +1,15 @@
 package jobsheet11;
 
 public class pengunjungcafe06 {
-    
+    static void daftarpengunjung(String...namapengunjung){
+        System.out.println("Daftar Pengunjung Kafe:");
+        for(int i = 0; i < namapengunjung.length; i++){
+            System.out.println("- " + namapengunjung[i]);
+        }
+    }
+    public static void main(String[] args) {
+        daftarpengunjung("ali", "Budi", "Citra");
+        daftarpengunjung("Andi");
+        daftarpengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
 }
