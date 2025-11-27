75fb03419a8d9e24b8c0a010b6c5e1ae33918fc9
carlosrama <carlosrama1144@gmail.com>
2025-11-27 13:47:57 +0700

    tugas 1

--- jobsheet11/tugas1.java
+++ jobsheet11/tugas1.java
@@ -1,5 +1,21 @@
 package jobsheet11;
 
 public class tugas1 {
-    
+    public double hitungvolumekubus(double sisi){
+        return sisi * sisi * sisi;
+    }
+    public double hitungluarpermukaankubus(double sisi){
+        return 6 * sisi * sisi;
+    }
+    public static void main(String[] args) {
+        tugas1 kubus = new tugas1();
+        double sisi = 5.0;
+
+        double volume = kubus.hitungvolumekubus(sisi);
+        double luasPermukaan = kubus.hitungluarpermukaankubus(sisi);
+
+        System.out.println("Sisi Kubus: " + sisi);
+        System.out.println("Volume Kubus: " + volume);
+        System.out.println("Luas Permukaan Kubus: " + luasPermukaan);
+    }
 }
